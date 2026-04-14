af3c7827424670a36da7771decb46b6fbe670ec6
novakro <novakro@student.uzlabina.cz>
2026-04-15 00:00:38 +0200

    fix authOptions import path

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -1,6 +1,6 @@
 import { GetServerSidePropsContext } from "next"
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "./api/auth/[...nextauth]"
+import { authOptions } from "../api/auth/[...nextauth]"
 
 export default function Home() {
     return null
