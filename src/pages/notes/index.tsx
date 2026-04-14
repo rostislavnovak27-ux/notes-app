99d5ff95711280a6d97c8e7791ec4a0436b7251f
novakro <novakro@student.uzlabina.cz>
2026-04-15 00:05:07 +0200

    fix notes auth redirect

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -2,8 +2,12 @@ import { GetServerSidePropsContext } from "next"
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../api/auth/[...nextauth]"
 
-export default function Home() {
-    return null
+export default function NotesPage() {
+    return (
+        <div>
+            <h1>Poznámky</h1>
+        </div>
+    )
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
@@ -19,9 +23,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     }
 
     return {
-        redirect: {
-            destination: "/notes",
-            permanent: false,
-        },
+        props: {},
     }
 }
